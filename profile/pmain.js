40e26c52719219e4797fbd1009d1c685bcc8859a
Nischal-Xenon <nischals772@gmail.com>
2025-04-29 20:19:45 +0545

    frist commit

---
+++ profile/pmain.js
@@ -0,0 +1,16 @@
+function like(){
+  document.getElementById('like').style.display = 'none';
+  document.getElementById('liked').style.display = 'block';
+}
+function liked(){
+  document.getElementById('liked').style.display = 'none';
+  document.getElementById('like').style.display = 'block';
+}
+function com(){
+  document.getElementById('com').style.display = 'none';
+  document.getElementById('comm').style.display = 'block';
+}
+function comm(){
+  document.getElementById('comm').style.display = 'none';
+  document.getElementById('com').style.display = 'block';
+}
